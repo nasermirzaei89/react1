e40754ec91232107cb0b9e2a0e22f67d2b2fe221
Naser Mirzaei <nasermirzaei89@gmail.com>
2024-01-29 00:11:21 -0500

    Add eslint

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 import React from "react";
 
-// @ts-ignore
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root')!)
 
 root.render(
     <React.StrictMode>
