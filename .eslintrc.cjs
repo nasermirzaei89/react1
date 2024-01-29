d5c528e89ad1d498e8bad2269bff6d1da8d6132f
Naser Mirzaei <nasermirzaei89@gmail.com>
2024-01-29 00:26:53 -0500

    Add eslint-plugin-react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,16 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
