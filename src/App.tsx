4440d1fd4ee37812b230b616def2a119db75086d
Naser Mirzaei <nasermirzaei89@gmail.com>
2024-01-29 00:21:18 -0500

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-    return (<h1>Hello Future</h1>)
+  return <h1>Hello Future</h1>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import React from "react";
 
-const root = createRoot(document.getElementById('root')!)
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
